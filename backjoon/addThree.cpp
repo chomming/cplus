f3e4c7fd87082f44c740b93e9c512b6c7fed5c2f
chomming <81208053+chomming@users.noreply.github.com>
2023-08-17 15:57:16 +0900

    Create addThree.cpp

---
+++ backjoon/addThree.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	long long A, B, C;
+	
+	std::cin >> A >> B >> C;
+	
+	long long sum = A + B + C;
+	
+	std::cout << sum << std::endl;
+	 
+	return 0;
+}
