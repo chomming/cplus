117336e2224d655d5b47762cc0e862799acd0d57
chomming <81208053+chomming@users.noreply.github.com>
2023-08-18 14:31:55 +0900

    Create grade.cpp

---
+++ backjoon/grade.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+int main() {
+	int A;
+	
+	std::cin >> A;
+
+	if (A >= 90) {
+		std::cout << "A" << std::endl;
+	} else if (A >= 80) { 
+		std::cout << "B" << std::endl;
+	} else if (A >= 70) {
+		std::cout << "C" << std::endl;
+	} else if (A >= 60) {
+		std::cout << "D" << std::endl;
+	} else {
+		std::cout << "F" << std::endl;
+	}
+	
+	return 0;
+}
