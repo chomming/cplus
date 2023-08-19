d2c542d7d7677cdf4db04bc187f5b415c4e7d5d7
chomming <81208053+chomming@users.noreply.github.com>
2023-08-19 18:09:02 +0900

    Create zeroEscape.cpp

---
+++ backjoon/zeroEscape.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+	int A, B;
+	
+	while(true) {
+		std::cin >> A >> B;
+	
+		if(A == 0 && B == 0) break;
+		
+		std:: cout << A+B << std::endl;	
+	}
+		
+	return 0;
+}
